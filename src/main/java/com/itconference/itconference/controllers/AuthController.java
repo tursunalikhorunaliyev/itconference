fb105c7ed1e55b07c13ca4db5b6a775baa6730c4
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-04-02 16:43:06 +0500

    .

---
+++ src/main/java/com/itconference/itconference/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.itconference.itconference.controllers;
+import com.itconference.itconference.model.ResultModel;
+import com.itconference.itconference.repositories.UsersCountRepository;
+import com.itconference.itconference.repositories.UsersRepository;
+import com.itconference.itconference.services.UserLoginService;
+import com.itconference.itconference.services.UserRegisterService;
+import com.itconference.itconference.services.UsersService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("it-conference-kuva/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final UserRegisterService userRegisterService;
+    private final UserLoginService userLoginService;
+
+
+    @PostMapping("register")
+    public ResponseEntity<ResultModel> register(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname, @RequestParam("phone") String phone, @RequestParam(required = false ,value = "device") String os){
+        return userRegisterService.register(firstname, lastname, phone, os);
+    }
+    @PostMapping("login")
+    public ResponseEntity<ResultModel> login(@RequestParam("phone") String phone){
+        return userLoginService.login(phone);
+    }
+
+
+}
--- src/main/java/com/itconference/itconference/controllers/UsersController.java
+++ src/main/java/com/itconference/itconference/controllers/UsersController.java
@@ -1,56 +1,41 @@
 package com.itconference.itconference.controllers;
+
 import com.itconference.itconference.entities.Users;
-import com.itconference.itconference.entities.UsersCount;
-import com.itconference.itconference.model.PageableContentModel;
 import com.itconference.itconference.model.ResultModel;
 import com.itconference.itconference.repositories.UsersCountRepository;
 import com.itconference.itconference.repositories.UsersRepository;
-import com.itconference.itconference.services.UserLoginService;
-import com.itconference.itconference.services.UserRegisterService;
 import com.itconference.itconference.services.UsersService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("it-conference-kuva/auth")
+@RequestMapping("it-conference-kuva/users")
 @AllArgsConstructor
 public class UsersController {
-    private final UserRegisterService userRegisterService;
-    private final UserLoginService userLoginService;
     private final UsersRepository usersRepository;
     private final UsersService usersService;
-
-    private final UsersCountRepository usersCountRepository;
-
-    @PostMapping("register")
-    public ResponseEntity<ResultModel> register(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname, @RequestParam("phone") String phone, @RequestParam(required = false ,value = "device") String os){
-        return userRegisterService.register(firstname, lastname, phone, os);
-    }
-    @PostMapping("login")
-    public ResponseEntity<ResultModel> login(@RequestParam("phone") String phone){
-        return userLoginService.login(phone);
-    }
 
     @GetMapping("all-v1users")
     public ResponseEntity<List<Users>> allv1Users(){
         return ResponseEntity.ok(usersRepository.findAll());
     }
 
-    @GetMapping("users")
+    @GetMapping("paging")
     public ResponseEntity<ResultModel> users(@RequestParam int page, @RequestParam int pageSize){
         return usersService.userByPage(page, pageSize);
     }
 
-    @GetMapping("users/today")
+    @GetMapping("today")
     public ResponseEntity<List<Users>> today(){
         return usersService.today();
     }
-    @GetMapping("users/yesterday")
+    @GetMapping("yesterday")
     public ResponseEntity<List<Users>> yesterday(){
         return usersService.yesterday();
 
