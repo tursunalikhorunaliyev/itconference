8f4e7303741d6acc290fc08b2b48ed8f332ae6f0
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2023-04-03 10:13:47 +0500

    .

--- src/main/java/com/itconference/itconference/services/UserRegisterService.java
+++ src/main/java/com/itconference/itconference/services/UserRegisterService.java
@@ -13,10 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -30,7 +26,7 @@ public class UserRegisterService {
 
 
 
-    public ResponseEntity<ResultModel> register(String firstname, String lastname, String phone, String os, String currentDate){
+    public ResponseEntity<ResultModel> register(String firstname, String lastname, String phone, String os){
 
 
         UserValidation userValidation = new UserValidation(usersRepository);
